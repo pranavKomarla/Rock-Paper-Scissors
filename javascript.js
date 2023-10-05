bce836df8ab8b8f321cd7f6abd93c496e8c4eada
Pranav Komarla <pranavkv03@gmail.com>
2023-10-05 17:22:43 -0400

    Added the UI

--- javascript.js
+++ javascript.js
@@ -1,3 +1,48 @@
+const body = document.querySelector("body");
+const div = document.createElement("div");
+
+const buttonRock = document.createElement("button");
+buttonRock.textContent = "Rock";
+
+const buttonPaper = document.createElement("button");
+buttonPaper.textContent = "Paper";
+
+const buttonScissors = document.createElement("button");
+buttonScissors.textContent = "Scissors";
+
+const resultsDiv = document.querySelector(".results");
+
+body.appendChild(div);
+div.appendChild(buttonRock);
+div.appendChild(buttonPaper);
+div.appendChild(buttonScissors);
+
+let playerSelection = "rock";
+
+buttonRock.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    playerSelection = "rock";
+    playRound(playerSelection, computerSelection);
+
+
+
+});
+
+buttonPaper.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    playerSelection = "paper";
+    playRound(playerSelection, computerSelection);
+});
+
+buttonScissors.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    playerSelection = "scissors";
+    playRound(playerSelection, computerSelection);
+});
+
+
+
+
 function getComputerChoice() {
 
     let randNum = Math.floor(Math.random() * 3) + 1;
@@ -9,7 +54,7 @@ function getComputerChoice() {
         case 2:
             return "Paper".toLowerCase();
             break;
-        case 1:
+        case 3:
             return "Scissors".toLowerCase();
             break;            
     }
@@ -22,24 +67,49 @@ function playRound(playerSelection, computerSelection) {
 
     console.log("Player: %s, Computer: %s", playerSelection.toLowerCase(), computerSelection);
 
+    let p = document.createElement("p");
+    resultsDiv.appendChild(p);
+
     let numCompSelection = 0;
-    if(playerSelection.toLowerCase() == "rock" && computerSelection == "scissors")
-        return "You Win! Rock beats Scissors!"
-    else if(playerSelection.toLowerCase() == "rock" && computerSelection == "paper")
-        return "You Lose! Paper beats Rock!"
-    else if(playerSelection.toLowerCase() == "paper" && computerSelection == "scissors")
-        return "You Lose! Scissors beats Paper!"
-    else if(playerSelection.toLowerCase() == "paper" && computerSelection == "rock")
-        return "You Win! Paper beats Rock!"
-    else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "paper")
-        return "You Win! Scissors beats Paper!"
-    else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "rock")
-        return "You Lose! Rock beats Scissors!"
-    else
-        return "You Tied!"
+    if(playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+        p.textContent = "You Win! Rock beats Scissors!";
+    } 
+        
+    else if(playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+        p.textContent = "You Lose! Paper beats Rock!";
+    }
+        
+    else if(playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+        p.textContent = "You Lose! Scissors beats Paper!";
+    }
+        
+    else if(playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+        p.textContent = "You Win! Paper beats Rock!";
+    }
+        
+    else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+        p.textContent = "You Win! Scissors beats Paper!";
+    }
+        
+    else if(playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+        p.textContent = "You Lose! Rock beats Scissors!";
+    }
+    else {
+        p.textContent = "You Tied!";
+
+    }
+       
+    
 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+
+
+
+
+
+
